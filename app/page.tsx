b340c095edb61365a04328de7fcaab1aceda9aa4
Deepanshu <admin@Admins-MacBook-Pro.local>
2024-06-14 12:48:37 +0530

    Hero Section Initialized

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className = "relative bg-black-100 items-center flex-col justify-center mx-auto sm:px-10 px-5">
+      <div className = "max-w-7xl w-full">
+        <h1>
+          Hello...
+        </h1>
+      </div>
+    </main>
+  );
+}
