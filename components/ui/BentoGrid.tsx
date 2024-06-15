8e5d36ed3dcfc7eec627dcebe35ef3a4f06c495f
Deepanshu <admin@Admins-MacBook-Pro.local>
2024-06-15 13:11:45 +0530

    Fixed spacing issue in bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -12,7 +12,8 @@ export const BentoGrid = ({
   return (
     <div
       className={cn(
-        "grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-5 md:grid-rows-7 gap-4 lg:gap-8 max-w-7xl mx-auto w-full py-20",
+        //"grid md:auto-rows-[18rem] grid-cols-1 md:grid-cols-5 md:grid-rows-7 gap-4 lg:gap-8 max-w-7xl mx-auto w-full py-20",
+        "grid grid-cols-1 md:grid-cols-6 lg:grid-cols-5 md:grid-row-7 gap-4 lg:gap-8 mx-auto",
         className
       )}
     >
@@ -43,7 +44,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 relative overflow-hidden rounded-3xl group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 ",
+        "row-span-1 relative overflow-hidden rounded-3xl border border-white/[0.1] group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none justify-between flex flex-col space-y-4",
         className
 
       )}
@@ -79,29 +80,30 @@ export const BentoGridItem = ({
           </BackgroundGradientAnimation>
         )}
 
-        <div className={cn(titleClassName, " group-hover/bento:translate-x-2 transition duration-200 relative md:hd-full min-h-40 px-5 p-5 lg:p-10 flex flex-col")}>
+        <div className={cn(titleClassName, " group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 px-5 p-5 lg:p-10 flex flex-col")}>
           <div className="font-sans font-extralight text-[#c1c2d3] text-sm md:text-xs lg:text-base">
             {description}
           </div>
           <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
             {title}
           </div>
-        </div>
-        {id === 2 && (
-          <GridGlobe />
-        )}
-        {id === 3 && (
-          <div className="flex gap-1 lg:gap-5 absolute -right-3 lg:-right-2 w-fit">
-            <div className="flex flex-col gap-3 lg:gap-8">
-              {['React', 'Next.js', 'TypeScript'].map((item) => (
+
+          {id === 2 && (
+            <GridGlobe />
+          )}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 absolute -right-3 lg:-right-2 w-fit">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {['React', 'Next.js', 'TypeScript'].map((item) => (
                   <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                     {item}
                   </span>
                 )
-              )}
+                )}
+              </div>
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
